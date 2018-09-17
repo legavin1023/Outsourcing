87609f3c3374a6f7489966e8680fdb9c452ed380
김희선 <legavin1023@gmail.com>
2018-09-18 05:41:58 +0900

    합체

--- js/M2.js
+++ js/M2.js
@@ -1,23 +1,10 @@
 $(document).ready(function() {
   var photoTarget = $('.m2Target >img');
-  var textTarget =$('.m2Target >div> dl');  
+  var textTarget1 =$('.m2Target >div> .pho-1');
+  var textTarget2 =$('.m2Target >div> .pho-2');
+  var textTarget3 =$('.m2Target >div> .pho-3');
   var btnTarget = $('.m2Target > div > ul> li>a');
   
-    
-     $(textTarget).hide();
-    $(".pho_1").show();
-     $("#pho-1").click(function(){
-         $(textTarget).hide();
-        $(".pho_1").show();
-    });
-    $("#pho-2").click(function(){
-        $(textTarget).hide();
-        $(".pho_2").show();
-    }); 
-    $("#pho-3").click(function(){
-        $(textTarget).hide();
-        $(".pho_3").show();
-    });
   btnTarget.click(function() {
     var srcValue = 'image/M2/' + $(this).attr('id') + '.jpg';
     if (!$(this).hasClass('active')) {
@@ -26,9 +13,22 @@ $(document).ready(function() {
       photoTarget.stop().fadeOut('fast',function() {
         $(this).attr('src', srcValue);
         $(this).stop().fadeIn('fast');
-      }); 
+      });
+    if(!$('#pho-1').hasClass('active')){
+    textTarget1.removeClass('hide');
+    textTarget2.addClass('hide');
+    textTarget3.addClass('hide');
+    };
+    if(!$('#pho-2').hasClass('active')){
+    textTarget2.removeClass('hide');
+    textTarget1.addClass('hide');
+    textTarget3.addClass('hide');
+    };
+    if(!$('#pho-3').hasClass('active')){
+    textTarget3.removeClass('hide');
+    textTarget1.addClass('hide');
+    textTarget2.addClass('hide');
+   };
     }
   })
-   
-   
 })
